438cc56b3c3a397d1b9039323523daf1f6848b37
Vladislav Panin <jone.cina1982@gmail.com>
2026-03-10 20:53:59 +0300

    fix: add board.png and delete locale

--- assets/js/group.js
+++ assets/js/group.js
@@ -1,26 +1,5 @@
 const groups = [
   {
-    "Name": "main",
-    "Title": "Общий чат",
-    "Description": "Общий чат, доступен всем кто зашёл",
-    "AvatarUrl": "https://asyncker.github.io/Board/assets/img/main.jpg"
-  },
-  {
-    "Name": "group",
-    "Title": "Предложка",
-    "Description": "Скидывайте в этот чат ссылки на другие обсуждение тем",
-    "AvatarUrl": "https://asyncker.github.io/Board/assets/img/group.jpg"
-  },
-  {
-    "Name": "test-group",
-    "Title": "Тестовый",
-    "Description": "Вы можете тестировать как хотите эту группу для демонстрации багов/спама/уязвимостей и другого, банов тут нет",
-    "AvatarUrl": "https://asyncker.github.io/Board/assets/img/test.jpg"
-  }
-];
-
-const groupsEng = [
-  {
     "Name": "maineng",
     "Title": "Free chat",
     "Description": "Free chat, send new message!",
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 let currentUserName = '';
 let currentUserNameColor = '';
 let currentUserAvatarUrl = '';
-let currentLocale = '';
 let currentPage = 0;
 let maxPage = 0;
 let currentGroupName = '';
@@ -30,25 +29,6 @@ messageInput.addEventListener('keydown', function(e) {
 });
 
 messageInput.addEventListener('input', autoResizeTextarea);
-
-function isSngTimezone() {
-    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const sngTimezones = [
-        'Europe/Moscow', 'Europe/Samara', 'Europe/Volgograd', 'Europe/Astrakhan',
-        'Europe/Ulyanovsk', 'Europe/Saratov', 'Europe/Kirov', 'Asia/Yekaterinburg', 
-        'Asia/Omsk', 'Asia/Krasnoyarsk', 'Asia/Irkutsk', 'Asia/Yakutsk', 
-        'Asia/Vladivostok', 'Asia/Sakhalin', 'Asia/Kamchatka', 'Asia/Novosibirsk', 
-        'Asia/Tomsk', 'Asia/Novokuznetsk', 'Asia/Barnaul', 'Asia/Chita', 
-        'Asia/Khandyga', 'Asia/Manchzhuria', 'Asia/Ust-Nera', 'Asia/Srednekolymsk',
-        'Asia/Anadyr', 'Europe/Kaliningrad', 'Europe/Simferopol', 'Europe/Minsk',
-        'Europe/Kiev', 'Europe/Uzhgorod', 'Europe/Zaporozhye', 'Europe/Simferopol',
-        'Asia/Yerevan', 'Asia/Almaty', 'Asia/Aqtau', 'Asia/Aqtobe', 'Asia/Atyrau', 
-        'Asia/Oral', 'Asia/Qostanay', 'Asia/Qyzylorda', 'Asia/Samarkand', 'Asia/Tashkent',
-        'Asia/Baku', 'Asia/Bishkek', 'Asia/Dushanbe', 'Asia/Ashgabat', 'Europe/Chisinau',
-        'Asia/Tbilisi'
-    ];
-    return sngTimezones.includes(timezone);
-}
 
 function getColorBySeed(seed, minColor = 100, maxColor = 256) {
   let hash = 2166136261;
@@ -84,8 +64,7 @@ function addGroupToHistory(groupName, groupTitle, groupAvatarUrl = '', isUpMessa
 function loadGroupsList() {
   try {
     const groupHistory = JSON.parse(localStorage.getItem('groupHistory') || '[]');
-    const groupHistoryList = isSngTimezone() ? groups : groupsEng;
-    const groupsList = groupHistory.length == 0 ? (currentLocale == "en" ? groupsEng : groupHistoryList) : groupHistory;
+    const groupsList = groupHistory.length == 0 ? groups : groupHistory;
     const contactList = document.querySelector('.contact-list');
     contactList.innerHTML = '';
     groupsList.forEach(group => {
@@ -360,8 +339,7 @@ messagesContainer.addEventListener('scroll', async function () {
 
 document.addEventListener('DOMContentLoaded', async function () {
   const urlParams = new URLSearchParams(window.location.search);
-  currentLocale = urlParams.get('locale') || 'ru';
-  let startGroup = isSngTimezone() ? groups[0] : groupsEng[0];
+  let startGroup = groups[0];
   currentGroupName = urlParams.get('group') || startGroup.Name;
   currentGroupTitle = urlParams.get('group') || startGroup.Title;
   currentUserName = localStorage.getItem('username');
